4b200eded2a4dc8c89bd0fd26febfc6f4d362bdb
kelvin ngure <kkarisjuiced@gmail.com>
2021-01-20 11:18:41 +0300

    creation of backend api

---
+++ App.js
@@ -0,0 +1,17 @@
+import { createAppContainer } from 'react-navigation'
+import { createStackNavigator } from 'react-navigation-stack'
+import HomeScreen from './src/screens/HomeScreen'
+
+const navigator = createStackNavigator(
+  {
+    Home: HomeScreen,
+  },
+  {
+    initialRouteName: 'Home',
+    defaultNavigationOptions: {
+      title: 'Virdis',
+    },
+  }
+)
+
+export default createAppContainer(navigator)
